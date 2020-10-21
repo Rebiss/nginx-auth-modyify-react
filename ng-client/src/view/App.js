83f86e05dcb474a726da23bb2ea907ad724be0c5
Andranik Barseghyan <barsegyanat.93@gmail.com>
2020-10-21 20:39:54 +0400

    init lib

--- ng-client/src/view/App.js
+++ ng-client/src/view/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import './App.css';
+import { SignUp } from './SignUp/SignUp'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-      </header>
-    </div>
+   <>
+    <SignUp />
+   </>
   );
 }
 
